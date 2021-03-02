1cdee5931f8c0978c42a3f8205970d6e4b15d452
Hamdi Sevitin <hamdi.sevitin@sap.com>
2021-03-02 15:52:25 +0200

    test

--- hello.js
+++ hello.js
@@ -4,15 +4,16 @@ const path = require('path')
 
 function sayHi(){
     const pwd = process.env['PWD'] || '.'
+    console.log("PWD ", pwd)
     // console.log(path.resolve('.'))
     // fs.readdirSync('.').forEach(file => {
     //     console.log(file);
     // });
     // console.log(path.resolve('./test.js'))
-    let data =  fs.readFileSync(`${pwd}/data/test.json`)
-    let result = JSON.parse(data)
-    console.log("hello!", result)
-    return result
+    // let data =  fs.readFileSync(`${pwd}/data/test.json`)
+    // let result = JSON.parse(data)
+    // console.log("hello!", result)
+    // return result
 }
 
 module.exports = sayHi
