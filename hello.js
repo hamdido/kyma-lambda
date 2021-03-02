75765c162a44c2c85dae07153d8bb90b1d919d5d
Hamdi Sevitin <hamdi.sevitin@sap.com>
2021-03-02 15:56:19 +0200

    user different env variable

--- hello.js
+++ hello.js
@@ -3,17 +3,17 @@ const fs = require('fs')
 const path = require('path')
 
 function sayHi(){
-    const pwd = process.env['PWD'] || '.'
+    const pwd = process.env['KUBELESS_INSTALL_VOLUME'] || '.'
     console.log("PWD ", pwd)
     // console.log(path.resolve('.'))
     // fs.readdirSync('.').forEach(file => {
     //     console.log(file);
     // });
     // console.log(path.resolve('./test.js'))
-    // let data =  fs.readFileSync(`${pwd}/data/test.json`)
-    // let result = JSON.parse(data)
-    // console.log("hello!", result)
-    // return result
+    let data =  fs.readFileSync(`${pwd}/data/test.json`)
+    let result = JSON.parse(data)
+    console.log("hello!", result)
+    return result
 }
 
 module.exports = sayHi
