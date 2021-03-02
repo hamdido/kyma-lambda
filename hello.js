47d85cb8346e24e7b8d18705fd0da66b1ed5502f
Hamdi Sevitin <hamdi.sevitin@sap.com>
2021-03-02 14:55:55 +0200

    test

--- hello.js
+++ hello.js
@@ -4,9 +4,9 @@ const path = require('path')
 
 function sayHi(){
     console.log(path.resolve('.'))
-    console.log(path.resolve('./test.js'))
-    let data =  fs.readFileSync('./test.js')
-    console.log("hello!", JSON.parse(data))
+    // console.log(path.resolve('./test.js'))
+    // let data =  fs.readFileSync('./test.js')
+    // console.log("hello!", JSON.parse(data))
 }
 
 module.exports = sayHi
