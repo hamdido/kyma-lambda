78d1751601a0a923aeca78db8041c5a2beedbc7b
Hamdi Sevitin <hamdi.sevitin@sap.com>
2021-03-02 14:51:54 +0200

    try with different extension

--- hello.js
+++ hello.js
@@ -4,8 +4,8 @@ const path = require('path')
 
 function sayHi(){
     console.log(path.resolve('.'))
-    console.log(path.resolve('./test.json'))
-    let data =  fs.readFileSync('./test.json')
+    console.log(path.resolve('./test.js'))
+    let data =  fs.readFileSync('./test.js')
     console.log("hello!", JSON.parse(data))
 }
 
