c7f8bc2798a96c9a83ae81d8a1d538b5bcada7fe
Hamdi Sevitin <hamdi.sevitin@sap.com>
2021-03-02 15:46:29 +0200

    user env variable for path

--- hello.js
+++ hello.js
@@ -3,13 +3,16 @@ const fs = require('fs')
 const path = require('path')
 
 function sayHi(){
-    console.log(path.resolve('.'))
-    fs.readdirSync('.').forEach(file => {
-        console.log(file);
-      });
+    const pwd = process.env['PWD'] || '.'
+    // console.log(path.resolve('.'))
+    // fs.readdirSync('.').forEach(file => {
+    //     console.log(file);
+    // });
     // console.log(path.resolve('./test.js'))
-    // let data =  fs.readFileSync('./test.js')
-    // console.log("hello!", JSON.parse(data))
+    let data =  fs.readFileSync(`${pwd}/data/test.json`)
+    let result = JSON.parse(data)
+    console.log("hello!", result)
+    return result
 }
 
 module.exports = sayHi
