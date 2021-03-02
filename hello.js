3c89d8ab43bed048f1f8a3e5f5a672d4b945ebfd
Hamdi Sevitin <hamdi.sevitin@sap.com>
2021-03-02 14:56:45 +0200

    show files

--- hello.js
+++ hello.js
@@ -4,6 +4,9 @@ const path = require('path')
 
 function sayHi(){
     console.log(path.resolve('.'))
+    fs.readdirSync('.').forEach(file => {
+        console.log(file);
+      });
     // console.log(path.resolve('./test.js'))
     // let data =  fs.readFileSync('./test.js')
     // console.log("hello!", JSON.parse(data))
